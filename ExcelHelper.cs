b4473ad42b86d5d2b2aee99932f65bec4b71c103
Think <aslyr2020@aliyun.com>
2019-10-03 21:12:46 +0800

    添加项目文件。

---
+++ ExcelHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Wpfnetframework
+{
+    public class ExcelHelper
+    {
+        private dynamic cell;
+
+        private dynamic sheet;
+
+        private bool visible = false;
+
+        /// <summary>
+        /// excel帮助类Com实现
+        /// </summary>
+        /// <param name="filepath">filepath如果为空，则自动添加一个新excel文件</param>
+        public ExcelHelper(string filepath = null)
+        {
+            Type excelType = Type.GetTypeFromProgID("Excel.Application");
+            dynamic excel = Activator.CreateInstance(excelType);
+            this.excel = excel;
+            excel.Visible = true;
+            if (filepath != null)
+            {
+                dynamic book = excel.WorkBooks.Open(filepath);
+            }
+            else
+            {
+                dynamic book = excel.WorkBooks.Add();
+            }
+            this.book = book;
+        }
+
+        /// <summary>
+        /// 工作䈬
+        /// </summary>
+        public dynamic book { get; set; }
+
+        /// <summary>
+        /// 获取单元格，默认参数为字符串“a1”第a列，第1行。
+        /// </summary>
+        public dynamic Cell
+        {
+            get { return cell; }
+
+            set
+            {
+                cell = sheet.Range(value);
+            }
+        }
+
+        /// <summary>
+        /// excel文件
+        /// </summary>
+        public dynamic excel { get; set; }
+        /// <summary>
+        /// 选取工作表，参数为int,从1开始
+        /// </summary>
+        public dynamic Sheet
+        {
+            get
+            {
+                return this.sheet;
+            }
+
+            set
+            {
+                sheet = excel.Worksheets(value);
+            }
+        }
+
+        public bool Visilbe
+        {
+            get { return excel.Visible; }
+            set { excel.Visible = value; }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Management;
+using System.IO;
+using System.Windows.Markup;
+using System.Windows.Controls;
+using System;
+namespace Wpfnetframework
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public MainWindow(string xamlFile)
+        {
+            InitializeComponent();
+            this.Title = "动态加载的xaml";
+            object gys根元素;
+            using (FileStream fs = new FileStream(xamlFile, FileMode.Open))
+            {
+                
+                gys根元素 = XamlReader.Load(fs);
+            }
+            this.Content = gys根元素;
+           
+        }
+        private void Button_Click(object sender,
+                                  RoutedEventArgs e)
+        {
+            //OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.ShowDialog();
+            //var filepath = openFileDialog.FileName;
+            //var excelhelper = new ExcelHelper(filepath);
+            //excelhelper.Sheet = 1;
+            //excelhelper.Visilbe = false;
+            //excelhelper.Cell = "i1";
+            //textbox1.AppendText(excelhelper.Cell.Text);
+            //SelectQuery query = new SelectQuery("Select * From PCI");
+            //var search = new ManagementObjectSearcher(query);
+            //foreach (var item in search.Get())
+            //{
+            //    textbox1.AppendText(item.ToString());
+            //}
+
+
+
+        }
+
+        private void textbox1_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            this.Title = ((TextBox)sender).SelectedText;
+        }
+
+        
+       
+
+        private void mainWinow_Loaded(object sender, RoutedEventArgs e)
+        {
+            combobox1.Items.Clear();
+            foreach (var item in Enum.GetValues(typeof( InkCanvasEditingMode)))
+            {
+                combobox1.Items.Add(item);
+            }
+            combobox1.SelectedIndex = 0;
+        }
+
+        private void combobox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            inkcanvas.EditingMode = (InkCanvasEditingMode)combobox1.SelectedItem;
+        }
+    }
+}
